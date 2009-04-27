68562de082dfd7a167ae715d2c16efb30b9e6038
hellocoton <hellocoton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-27 06:58:42 +0000

    v1 widget wp hellocoton
    git-svn-id: https://plugins.svn.wordpress.org/hellocoton/trunk@113640 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hellocoton.php
@@ -0,0 +1,38 @@
+<?php
+/*
+Plugin name: Hellocoton
+Plugin URI: http://www.hellocoton.fr/
+Author: Team Hellocoton
+Author URI: http://www.hellocoton.fr/
+Version: 0.1
+Description: Ajoute un bouton "J'aime" par hellocoton.
+
+INSTALL :
+L'installation est automatique ! C'est terminé :)
+
+*/
+
+add_filter('the_content', 'hellocoton_hook');
+add_filter('the_excerpt', 'hellocoton_hook');
+
+function hellocoton_html() {
+	
+	$articleUrl = htmlspecialchars(strip_tags(get_permalink()),ENT_QUOTES);
+	
+	return '<script src="http://www.hellocoton.fr/widgets/js.php?url='.$articleUrl.'" type="text/javascript"></script>
+	<noscript>Venez voter pour cet article sur <a href="http://www.hellocoton.fr">hellocoton</a></noscript>';
+	
+}
+
+function hellocoton_hook($content='') {
+
+	if (is_feed()) {
+		$content .= hellocoton_html();
+	} else {
+		$content .= hellocoton_html();
+	}
+	return $content;
+
+}
+
+?>
