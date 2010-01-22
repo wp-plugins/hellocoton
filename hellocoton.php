e757cb60bb49a5ca6b9c9f11ff97c5c127130e4f
hellocoton <hellocoton@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-01-22 10:17:32 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/hellocoton/tags/1.0@196552 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- hellocoton.php
+++ hellocoton.php
@@ -4,13 +4,16 @@ Plugin name: Hellocoton
 Plugin URI: http://www.hellocoton.fr/
 Author: Team Hellocoton
 Author URI: http://www.hellocoton.fr/
-Version: 0.2
+Version: 1.0
 Description: Ajoute un bouton "J'aime cet article" par hellocoton.
 
 INSTALL :
 L'installation est automatique ! C'est terminé :)
 
 */
+
+if(!defined('WP_CONTENT_URL')) define('WP_CONTENT_URL',get_option('siteurl').'/wp-content');
+if(!defined('WP_PLUGIN_URL')) define('WP_PLUGIN_URL',WP_CONTENT_URL.'/plugins');
 
 add_filter('the_content', 'hellocoton_hook');
 add_filter('the_excerpt', 'hellocoton_hook');
@@ -21,16 +24,38 @@ function hellocoton_html() {
 	
 	$rand = rand(0,1000000);
 	
-	$return = '<span id="hellocoton_'.$rand.'" style="display:block;width:147px;height:26px;position:relative;padding:0;border:10px 0px;margin:0;clear:both;"><a href="#" onclick="javascript:return false;"  style="display:block;width:120px;height:26px;position:absolute;top:0;left:0;padding:0;border:0;margin:0;"><img src="http://www.hellocoton.fr/widgets/img/action-on-h.gif" border="0" style="padding:0;border:0;margin:0;float:none;" /></a><a href="http://www.hellocoton.fr" target="_blank" style="display:block;width:27px;height:26px;position:absolute;top:0;left:120px;"><img src="http://www.hellocoton.fr/widgets/img/hellocoton.gif" border="0" alt="Rendez-vous sur Hellocoton !" style="padding:0;border:0;margin:0;float:none;" /></a></span>';
+	$wpurl = WP_PLUGIN_URL."/hellocoton/";
+
+	$return = '<span id="hellocoton_'.$rand.'" style="display:block;width:147px;height:30px;position:relative;padding:0;border:10px 0px;margin:0;clear:both;">
+	<a id="hellocoton_load_'.$rand.'" href="#" onclick="javascript:return false;"  style="display:block;width:120px;height:30px;position:absolute;top:0;left:0;padding:0;border:0;margin:0;"><img src="'.$wpurl.'loading.gif" border="0" style="padding:0;border:0;margin:0;float:none;" /></a>
+	<a id="hellocoton_vote_'.$rand.'" href="#" onclick="return false;"  style="display:none;width:120px;height:30px;position:absolute;top:0;left:0;padding:0;border:0;margin:0;"><img src="'.$wpurl.'action-on.gif" border="0" style="padding:0;border:0;margin:0;float:none;" onmouseover="javascript:this.src=\''.$wpurl.'action-on-h.gif\'" onmouseout="javascript:this.src=\''.$wpurl.'action-on.gif\'" /></a>
+	<a id="hellocoton_unvote_'.$rand.'" href="#" onclick="javascript:return false;"  style="display:none;width:120px;height:30px;position:absolute;top:0;left:0;padding:0;border:0;margin:0;"><img src="'.$wpurl.'action-off.gif" border="0" style="padding:0;border:0;margin:0;float:none;" onmouseover="javascript:this.src=\''.$wpurl.'action-off-h.gif\'" onmouseout="javascript:this.src=\''.$wpurl.'action-off.gif\'" /></a>
+	<a href="http://www.hellocoton.fr" target="_blank" style="display:block;width:27px;height:30px;position:absolute;top:0;left:120px;"><img src="'.$wpurl.'hellocoton.gif" border="0" alt="Rendez-vous sur Hellocoton !" style="padding:0;border:0;margin:0;float:none;" /></a></span>';
 	
-	return $return.'<script src="http://www.hellocoton.fr/widgets/js.php?uniq='.$rand.'&url='.$articleUrl.'" type="text/javascript"></script>';
+	return $return.'<script type="text/javascript">hellocoton_plugin_url="'.$wpurl.'"</script><script src="http://widget.hellocoton.fr/widget05.js?uniq='.$rand.'&url='.$articleUrl.'" type="text/javascript"></script>';
+	
+}
+
+function hellocoton_feed_html() {
+	
+	$articleUrl = urlencode(strip_tags(get_permalink()));
+	
+	$rand = rand(0,1000000);
+	
+	$wpurl = WP_PLUGIN_URL."/hellocoton/";
+
+	$return = '<span id="hellocoton_'.$rand.'" style="display:block;width:147px;height:30px;position:relative;padding:0;border:10px 0px;margin:0;clear:both;">
+	<a id="hellocoton_vote_'.$rand.'" href="http://www.hellocoton.fr/vote?url='.$articleUrl.'" style="display:block;width:120px;height:30px;position:absolute;top:0;left:0;padding:0;border:0;margin:0;"><img src="'.$wpurl.'action-on.gif" border="0" style="padding:0;border:0;margin:0;float:none;" onmouseover="javascript:this.src=\''.$wpurl.'action-on-h.gif\'" onmouseout="javascript:this.src=\''.$wpurl.'action-on.gif\'" /></a>
+	<a href="http://www.hellocoton.fr" target="_blank" style="display:block;width:27px;height:30px;position:absolute;top:0;left:120px;"><img src="'.$wpurl.'hellocoton.gif" border="0" alt="Rendez-vous sur Hellocoton !" style="padding:0;border:0;margin:0;float:none;" /></a></span>';
+	
+	return $return;
 	
 }
 
 function hellocoton_hook($content='') {
 
 	if (is_feed()) {
-		$content .= hellocoton_html();
+		$content .= hellocoton_feed_html();
 	} else {
 		$content .= hellocoton_html();
 	}
